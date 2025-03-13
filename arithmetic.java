999d71875fc7e60eac8afd2f9859578626c00c89
sathyababisha <inshamanju20>
2025-03-13 11:11:44 +0530

    arithmetic

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+public class arithmetic{
+    public static void main(String[]args){
+     int X=10;
+     int Y=15;
+        System.out.println(X+Y);
+        System.out.println(X-Y);
+        System.out.println(X*Y);
+    }
+}
