c62511482aa172982f98265a183657eb2ef0f741
sathyababisha <inshamanju20>
2025-03-13 10:12:13 +0530

    test added

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test{
+    public static void main (String[]args){
+        String Name="sathya";
+        String Class="Computer science";
+        String Section="B";
+        int a1=62;
+        int a2=50;
+        int a3=90;
+        char Grade='a';
+        System.out.println("Name:"+ Name);
+        System.out.println("Class:"+ Class);
+        System.out.println("Section:"+ Section);
+        System.out.println("Total mark:"+ a1+a2+a3);
+        System.out.println("Grade:"+ Grade);
+    }
+}
