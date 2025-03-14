e48ff81c88f420ee747c7706c4ba5118ddd95a1c
sathyababisha <inshamanju20>
2025-03-14 10:56:40 +0530

    dowhilestring

---
+++ dowhilestring.java
@@ -0,0 +1,13 @@
+public class dowhilestring
+{
+    public static void main(String[]args)
+    {
+        int i=1;
+        do
+        {
+            System.out.println("apple");
+            i++;
+        }
+        while(i<=5);
+    }
+}
