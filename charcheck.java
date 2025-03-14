34df165074616b11f2743748fe7a0760fb380185
sathyababisha <inshamanju20>
2025-03-14 10:21:22 +0530

    charcheck

---
+++ charcheck.java
@@ -0,0 +1,24 @@
+public class charcheck
+{
+    public static void main(String[]args)
+    {
+        char c='a';
+        if(Character.isLetter(c))
+        {
+            if(c=='a'||c=='e'||c=='o'||c=='i'||c=='u')
+ {
+    System.out.println("It is vowel");
+ }
+ else
+ {
+    System.out.println("It is consonant");
+ }
+ }
+ else
+ {
+    System.out.println("It is not a charachter");
+ }
+ }
+ }
+
+
