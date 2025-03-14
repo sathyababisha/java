d26e4e943f49e03d0797d491524ee3871d57748c
sathyababisha <inshamanju20>
2025-03-14 13:21:58 +0530

    methods2

---
+++ methods.java
@@ -0,0 +1,18 @@
+public class methods
+{
+    static void mymethod()
+    {
+        int a=1;
+        int b=2;
+        int c=a*b;
+        int d=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("sum:"+d);
+        System.out.println("Product:"+c);
+    }
+    public static void main(String[]args)
+    {
+        mymethod();
+    }
+}
