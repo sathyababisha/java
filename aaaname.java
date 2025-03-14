9b5e39265c70826ff08a14079616dc495ac285b1
sathyababisha <inshamanju20>
2025-03-14 14:45:03 +0530

    array index change

---
+++ aaaname.java
@@ -0,0 +1,11 @@
+public class aaaname
+{
+    public static void main(String[]args)
+    {
+        String[] fruits={"kivi","mango","apple"};
+        fruits[0]="orange";
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+    }
+}
