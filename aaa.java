4f2432fdb8a46607b62bb3f39d1b40bab79cb0f3
sathyababisha <inshamanju20>
2025-03-14 13:58:04 +0530

    array

---
+++ aaa.java
@@ -0,0 +1,11 @@
+public class aaa
+{
+    public static void main(String[]args)
+    {
+        String[] fruits={"apple","kivi","mango"};
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+
+    }
+}
