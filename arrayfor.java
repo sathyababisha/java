46b61162e10cef3431c64a1454a30cc555387960
sathyababisha <inshamanju20>
2025-03-14 15:31:33 +0530

    arrays of fruits

---
+++ arrayfor.java
@@ -0,0 +1,11 @@
+public class arrayfor
+{
+    public static void main(String[]args)
+    {
+        String[]fruits={"Apple","kivi","mango","dragon","grapes"};
+        for(int i=0;i<=5;i++)
+        {
+            System.out.println(fruits[i]);
+        }
+    }
+}
