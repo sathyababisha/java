34ce1a2ba916af90ab784603d8cef6819fbf63fa
sathyababisha <inshamanju20>
2025-03-14 11:13:23 +0530

    forloop2

---
+++ forloop2.java
@@ -0,0 +1,11 @@
+public class forloop2
+{
+    public static void main(String[]args)
+    {
+        int i =15;
+        for(i=15;i>=10;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
