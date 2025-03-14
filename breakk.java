f66f7f08598c58969c6f420cdfc8dff3922206a5
sathyababisha <inshamanju20>
2025-03-14 11:43:36 +0530

    break

---
+++ breakk.java
@@ -0,0 +1,15 @@
+public class breakk
+{
+    public static void main(String[]args)
+    {
+        int i = 0;
+        for(i=0;i<=10;i++)
+        {
+            if(i==7)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
