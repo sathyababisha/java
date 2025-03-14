f8e3bb243d0a1cb8de1104ef1007af91d46b01c3
sathyababisha <inshamanju20>
2025-03-14 15:49:10 +0530

    classes

---
+++ classs.java
@@ -0,0 +1,17 @@
+class student
+    {
+        int id;
+        String name;
+    }
+        public class classs
+    {
+        public static void main(String[]args)
+        {
+            student s1=new student();
+            s1.id=101;
+            s1.name="sathya";
+            System.out.println(s1.name);
+            System.out.println(s1.id);
+
+        }
+    }
