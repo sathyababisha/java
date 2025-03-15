480694ecb5e150247fd36c61fc75661f4ab806ff
sathyababisha <inshamanju20>
2025-03-15 09:41:26 +0530

    compare2

---
+++ compare2.java
@@ -0,0 +1,13 @@
+public class compare2{
+    public static void main(String[]args){
+        Integer int1=5;
+        Integer int2 =7;
+        if(int1==int2)
+        {
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
