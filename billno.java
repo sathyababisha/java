33f7e0e243eaf320453b3751ac0474b40272c743
sathyababisha <inshamanju20>
2025-03-15 11:10:13 +0530

    int inheritance

---
+++ billno.java
@@ -0,0 +1,11 @@
+public class billno
+{
+    public static void main(String[]args)
+    {
+        int[]bill_nu={10,20,30,40,50};
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("bill_nu:"+bill_nu[i]);
+        }
+    }
+}
