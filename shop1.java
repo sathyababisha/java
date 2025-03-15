b96b206b72e92bb337771e46c09c0ca580eb5535
sathyababisha <inshamanju20>
2025-03-15 11:39:03 +0530

    inheritance values

---
+++ shop1.java
@@ -0,0 +1,12 @@
+public class shop1
+{
+    public static void main(String[]args)
+    {
+        String[] product_name={"biscut","tea","cffee","chocolate","puff","cookies"};
+        int[] price_amount={20,10,14,30,25,40};
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("product:"+product_name[i]+"price"+price_amount[i]);
+        }
+    }
+}
