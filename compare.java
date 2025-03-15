439579570d11a30cd76ef3052a49c09d007b6b16
sathyababisha <inshamanju20>
2025-03-15 09:39:22 +0530

    comparision operator

---
+++ compare.java
@@ -0,0 +1,14 @@
+public class compare{
+    public static void main(String[]args){
+        String Str1="sathya";
+        String Str2="babisha";
+        if(Str1==Str2)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+        }
+    }
