b79c20d20f4270c700d2e91417008677a9715ae9
sathyababisha <inshamanju20>
2025-03-15 09:37:15 +0530

    assignment operator

---
+++ assignment.java
@@ -0,0 +1,11 @@
+public class assignment{
+    public static void main(String[]args){
+     int X=10;
+     int Y=15;
+     int c=X+Y;
+     int d=X*Y;
+      System.out.println(c);
+        System.out.println(d);
+        
+    }
+}
